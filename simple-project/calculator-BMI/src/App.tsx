86dc14161b08f12876213ddcb7f0803118c66d7d
ngovanquoc4802000 <ngovanquoc480@gmail.com>
2024-05-21 21:52:07 +0700

    .

--- simple-project/calculator-BMI/src/App.tsx
+++ simple-project/calculator-BMI/src/App.tsx
@@ -1,56 +1,104 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /*
-import { useMemo, useState } from "react";
-import "./styles.css";
+import { useEffect, useMemo, useState } from "react";
+import "./index.css";
 
-const DEFAULT_WEIGHT = 50;
-const DEFAULT_HEIGHT = 150;
+export const SolieuWeight: number = 40;
+export const SolieuHeight: number = 150;
 
-export default function App() {
-  const [height, setHeight] = useState(DEFAULT_HEIGHT);
-  const [weight, setWeight] = useState(DEFAULT_WEIGHT);
 
-  function onHeightChange(event: { target: { value: any; }; }) {
-    const inputHeight = event.target.value;
-    setHeight(inputHeight);
+function App() {
+  const [weight, setWeight] = useState(SolieuWeight);
+  const [height, setHeight] = useState(SolieuHeight);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onWeightChange = (e: { target: { value: any; }; }) => {
+    const tarGetWeight = e.target.value;
+    setWeight(tarGetWeight);
   }
-
-  function onWeightChange(event: { target: { value: any; }; }) {
-    const inputWeight = event.target.value;
-    setWeight(inputWeight);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onHeightChange = (e: { target: { value: any; }; }) => {
+    const tarGetHeight = e.target.value;
+    setHeight(tarGetHeight)
   }
-
-  const output = useMemo(() => {
-    const calculatedHeight = height / 100;
-    return (weight / (calculatedHeight * calculatedHeight)).toFixed(1);
+  // Công thức tính BMI nặng / chiều cao bình phương
+  //BMI từ 18,5 den 24,9 bình thường
+  //BMI tu 25 den 30 là thừa cân 
+  // Nếu tính độ chuẩn của BMI thì vd: 1m72 (lấy 72 nhân với 9 chia  cho 10)
+  // => 1 người bình thường không chơi thể thao .
+  const Memo = useMemo(() => {
+    const total = height / 100;
+    return (weight / (total * total)).toFixed(1);
   }, [weight, height]);
-
   return (
     <main>
       <h1>Project 7: BMI CALCULATOR</h1>
-      <div className="input-section">
-        <p className="slider-output">Weight: {weight} kg</p>
-        <input
-          className="input-slider"
-          onChange={onWeightChange}
-          type="range"
-          step="1"
-          min="40"
-          max="220"
-        />
-        <p className="slider-output">Height: {height} cm</p>
-        <input
-          className="input-slider"
-          onChange={onHeightChange}
-          type="range"
-          min="140"
-          max="220"
-        />
-      </div>
-      <div className="output-section">
-        <p>Your BMI is</p>
-        <p className="output">{output}</p>
-      </div>
-    </main>
+      <div className="input-weight"  >
+        {/* State sẽ thay đổi khi lướt qua */
+/*
+<p style={{ textAlign: "left" }}>Weight: {weight} kg</p>
+<input
+  style={{ width: "150%" }}
+  type="range"
+  step="1"
+  min="40"
+  max="220"
+  onChange={onWeightChange}
+/>
+<p style={{ textAlign: "left" }}>Height: {height} cm</p>
+<input
+  style={{ width: "150%" }}
+  type="range"
+  step="1"
+  min="40"
+  max="220"
+  onChange={onHeightChange}
+/>
+</div>
+<div className="footer">
+<h4>Your BMI is</h4>
+<button>{Memo}</button>
+</div>
+</main>
+);
+}
+
+export default App;
+*/
+
+import { useState } from "react";
+import { UseEffectName } from "./DemoEffect";
+
+function DemoUseEffect() {
+  const [index, setIndex] = useState(0);
+  const [folder, setFolder] = useState(false);
+  const StateList = index < UseEffectName.length - 1;
+  const handleClickNext = () => {
+    if (StateList) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+  const handleShowDetails = () => {
+    setFolder(!folder)
+  }
+  const UseEffect = UseEffectName[index];
+  console.log(UseEffect)
+  return (
+    <div>
+      <button onClick={handleClickNext} >Next</button>
+      <h2 style={{ display: "flex", alignItems: "center" }}>
+        <i>{UseEffect.name} </i>
+        by
+        <h3> {UseEffect.artist}</h3>
+      </h2>
+      <button onClick={handleShowDetails} >
+        {folder ? "hide" : "show"} details
+      </button>
+      {folder && <h2>{UseEffect.description}</h2>}
+      <img src={UseEffect.url} alt="hinh anh" />
+    </div>
   );
 }
-*/
+
+export default DemoUseEffect;
--- simple-project/calculator-BMI/src/main.tsx
+++ simple-project/calculator-BMI/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App1 from './App1.tsx'
+import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App1/>
+    <App/>
   </React.StrictMode>,
 )
