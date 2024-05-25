4e1f7fd269d050dec758b72909cf2494f75c7b9d
ngovanquoc4802000 <ngovanquoc480@gmail.com>
2024-05-25 12:51:23 +0700

    commit form build 25/5 vẫn còn thiếu remove array theo thứ tự

--- simple-project/calculator-BMI/src/formMain.tsx
+++ simple-project/calculator-BMI/src/formMain.tsx
@@ -1,62 +1,134 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
-import "./formMain.css";
-
-interface FormTs {
-  Name: string,
-  Email: string,
-  HomeTelephone: string
-}
-
-const Name: FormTs = {
-  Name: "",
-  Email: "",
-  HomeTelephone: "",
-}
-function FormMani() {
+import './formMain.css';
+import ValidationError from "./validationError";
+function FormMain() {
   const [arrSend, setArrSend] = useState([]);
-  const [answer, setAnswer] = useState(Name);
-  const handleInput = (e: { target: { name: string; value: string; }; }) => {
-    setAnswer({ ...answer, [e.target.name]: e.target.value })
+  const [validationerr, setValidationerr] = useState({});
+  const [object, setObject] = useState({
+    id: 0,
+    Name: "",
+    Date: "",
+    email: "",
+    Address: "",
+    HomeTelephone: "",
+  })
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setValidationerr(ValidationError({ object }));
+  }
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setObject({ ...object, [name]: value })
   }
 
-  const handleSubmit = (e: { preventDefault: () => void; }) => {
-    e.preventDefault();
-  }
-  const { name, Email, HomeTelephone } = answer;
-  console.log(Name);
   const handleSend = () => {
-    setArrSend([...arrSend, { Name, Email, HomeTelephone }])
-    console.log(arrSend)
+    const { Address, Date, Email, HomeTelephone } = object;
+    setArrSend([...arrSend, { Address, Date, Email, HomeTelephone }],);
+    console.log(arrSend);
+  };
+  const handleClose = () => {
+    setArrSend([])
   }
   return (
     <form onSubmit={handleSubmit}>
       <h1>Personal Information Form</h1>
-      <input type="text" autoComplete="off" value={answer.Name} name="Name" onChange={handleInput} placeholder="Name" />
-      <input type="text" autoComplete="off" value={answer.Email} name="Email" onChange={handleInput} placeholder="Email" />
-      <input type="text" autoComplete="off" value={answer.HomeTelephone} name="HomeTelephone" onChange={handleInput} placeholder="HomeTelephone" />
+      <label htmlFor="">Name</label>
+      <input
+        type="text"
+        autoComplete="off"
+        value={object.Name}
+        name="Name"
+        onChange={handleInput}
+        placeholder="Name"
+      />
+      {validationerr.name && <span style={{ color: "red" }} className="error">{validationerr.name}</span>}
+      <label htmlFor="">Date of Birth</label>
+      <input
+        type="text"
+        autoComplete="off"
+        value={object.Date}
+        name="Date"
+        onChange={handleInput}
+        placeholder="date"
+      />
+      {validationerr.date && <span style={{ color: "red" }} className="error">{validationerr.date}</span>}
+
+
+      <label htmlFor="">Email</label>
+      <input
+        type="text"
+        autoComplete="off"
+        value={object.Email}
+        name="email"
+        onChange={handleInput}
+        placeholder="Email"
+      />
+      {validationerr.email && <span style={{ color: "red" }} className="error">{validationerr.email}</span>}
+      <label htmlFor="">Address</label>
+      <input
+        type="text"
+        autoComplete="off"
+        value={object.Address}
+        name="Address"
+        onChange={handleInput}
+        placeholder="Street Address"
+      />
+      { validationerr.address && <span style={{color: "red"}} className="error">{validationerr.address}</span> }
+      <label htmlFor="">Home Telephone Number</label>
+      <input
+        type="text"
+        autoComplete="off"
+        value={object.HomeTelephone}
+        name="HomeTelephone"
+        onChange={handleInput}
+        placeholder="HomeTelephone"
+      />
+      {validationerr.hometelephone && <span style={{ color: "red" }} className="error">{validationerr.hometelephone}</span>}
       <button onClick={handleSend} className="send" type="submit">
         SEND
       </button>
       {/* arrSend.map sẽ ra 1 mẫu .. và close nút X */}
-       <div style={{ border: "1px solid black", marginTop: "2rem", borderRadius: "8px"}} >
-        <div style={{ display: "flex", alignItems: "center", justifyContent: "center", position: "relative"}} className="title-close">
-        <h2 style={{ textAlign: "center" }} >Information</h2>
-        <button style={{ position: "absolute", right: "3px"}} >X</button>
-        </div>
-         <ul style={{ listStyle: "none", marginRight: "24rem" }}>
-          <li>Name: </li>
-          <li>Date of Birth: </li>
-          <li>Address: </li>
-          <li>Gender: </li>
-          <li>Home Telephone Number: </li>
-          <li>Mobile Number: </li>
-          <li>Preferred Contact Address </li>
-         </ul>
-       </div>
-
+      {arrSend.map((item) => {
+        return (
+          <div
+            style={{
+              border: "1px solid black",
+              marginTop: "2rem",
+              borderRadius: "8px",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                position: "relative",
+              }}
+              className="title-close"
+            >
+              <h2 style={{ textAlign: "center" }}>Information</h2>
+              <button
+                onClick={handleClose}
+                style={{ position: "absolute", right: "3px" }}
+              >
+                X
+              </button>
+            </div>
+            <ul style={{ listStyle: "none", marginRight: "24rem" }}>
+              <li>Name: {item.name}</li>
+              <li>Date of Birth: {item.Date}</li>
+              <li>Email: {item.Email}</li>
+              <li>Email: {item.Address}</li>
+              <li>Gender: </li>
+              <li>Home Telephone Number: {item.HomeTelephone} </li>
+              <li>Mobile Number: </li>
+              <li>Preferred Contact Address </li>
+            </ul>
+          </div>
+        );
+      })}
     </form>
   );
 }
 
-export default FormMani;
+export default FormMain;
