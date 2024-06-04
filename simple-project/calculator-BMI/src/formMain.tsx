b1050043ff72b1d72bc7a389ecbe8a0091a49a7c
ngovanquoc4802000 <ngovanquoc480@gmail.com>
2024-06-04 17:48:30 +0700

    dev done... fix anh vương

--- simple-project/calculator-BMI/src/formMain.tsx
+++ simple-project/calculator-BMI/src/formMain.tsx
@@ -12,9 +12,9 @@ interface Data {
 export const data: Data[] = [
   {
     id: 0,
-    name: "",
-    email: "",
-    address: "",
+    name: "quoc",
+    email: "ngovanquoc480@gmail.com ",
+    address: "89A/3a, Đường Hải thượng lãng ông",
     packed: false
   },
 ]
@@ -26,7 +26,6 @@ function FormMain() {
   const [arrayForm, setArrayForm] = useState(data);
   const handleSubmit = (e) => {
     e.preventDefault();
-
   }
   const handleInput = (e) => {
     setName(e.target.value)
@@ -42,8 +41,12 @@ function FormMain() {
     setEmail('');
     setFirst('');
     if (email === "") {
-      return "Succes";
+      return "Success";
     }
+    if (arrayForm.length === 3) {
+      return "disable"
+    }
+
     setArrayForm([
       ...arrayForm,
       {
@@ -62,58 +65,92 @@ function FormMain() {
   return (
     <form onSubmit={handleSubmit}>
       <h1>Personal Information Form</h1>
-      <label htmlFor="">Email</label>
-      <input
-        type="text"
-        autoComplete="off"
-        value={name}
-        name="Name"
-        onChange={handleInput}
-        placeholder="Name"
-      />
-      <input
-        type="text"
-        autoComplete="off"
-        value={email}
-        name="email"
-        onChange={handleInputEmail}
-        placeholder="Email"
-      />
-
-      <input
-        type="text"
-        autoComplete="off"
-        value={first}
-        name="FirstName"
-        onChange={handleInputFirst}
-        placeholder="Address"
-      />
+      {
+        arrayForm.length !== 3 ? (
+          <>
+            <input
+              type="text"
+              autoComplete="off"
+              value={name}
+              name="Name"
+              onChange={handleInput}
+              placeholder="Name"
+            />
+            <input
+              type="text"
+              autoComplete="off"
+              value={email}
+              name="email"
+              onChange={handleInputEmail}
+              placeholder="Email"
+            />
+            <input
+              type="text"
+              autoComplete="off"
+              value={first}
+              name="FirstName"
+              onChange={handleInputFirst}
+              placeholder="Address"
+            />
+          </>
+        ) : (
+          <>
+            <input type="text" value={name} disabled placeholder="Name" />
+            <input type="text" value={email} disabled placeholder="Address" />
+            <input type="text" value={first} disabled placeholder="FirstName" />
+          </>
+        )
+      }
       <button disabled={email === ""} onClick={() => handleSend({ name, email, first })} className="send" type="submit">
         SEND
       </button>
       <ul>
-        {/* arrSend.map sẽ ra 1 mẫu .. và close nút X */}
-        {arrayForm.map((item, id) => {
-          return (
-            <li className="Title" style={{ listStyle: "none" }} key={id}>
-              <div className="Name-Title">
-                <label className="Name" htmlFor="">
-                  Name: {""}
-                  <span>{item.name}</span>
-                </label>
-                <label className="Name" htmlFor="">
-                  Email: {""}
-                  <span>{item.email}</span>
-                </label>
-                <label className="Name" htmlFor="">
-                  Address: {""}
-                  <span>{item.address}</span>
-                </label>
-              </div>
-              <button onClick={() => handleClose(item.id)} >X</button>
-            </li>
-          );
-        })}
+        {
+          arrayForm.length !== 3 ? (
+            arrayForm.map((item, id) => {
+              return (
+                <li className="Title" style={{ listStyle: "none" }} key={id}>
+                  <div className="Name-Title">
+                    <label className="Name" htmlFor="">
+                      Name: {""}
+                      <span>{item.name}</span>
+                    </label>
+                    <label className="Name" htmlFor="">
+                      Email: {""}
+                      <span>{item.email}</span>
+                    </label>
+                    <label className="Name" htmlFor="">
+                      Address: {""}
+                      <span>{item.address}</span>
+                    </label>
+                  </div>
+                </li>
+              )
+            })
+          ) : (
+            arrayForm.map((item, id) => {
+              return (
+                <li className="Title" style={{ listStyle: "none" }} key={id}>
+                  <div className="Name-Title">
+                    <label className="Name" htmlFor="">
+                      Name: {""}
+                      <span>{item.name}</span>
+                    </label>
+                    <label className="Name" htmlFor="">
+                      Email: {""}
+                      <span>{item.email}</span>
+                    </label>
+                    <label className="Name" htmlFor="">
+                      Address: {""}
+                      <span>{item.address}</span>
+                    </label>
+                  </div>
+                  <button onClick={() => handleClose(item.id)}>X</button>
+                </li>
+              )
+            })
+          )
+        }
       </ul>
     </form>
   );
