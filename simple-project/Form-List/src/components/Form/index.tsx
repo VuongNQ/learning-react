0a5ca94c28bd6daad1624a178769c5f1020b709b
ngovanquoc4802000 <ngovanquoc480@gmail.com>
2024-06-27 17:59:39 +0700

    .

--- simple-project/Form-List/src/components/Form/index.tsx
+++ simple-project/Form-List/src/components/Form/index.tsx
@@ -5,7 +5,6 @@ import TaskList from './TaskList';
 
 const FormSection = () => {
   const [tasks, setTask] = useState(dataList);
-  const [isActive1, setIsActive1] = useState(true);
   const handlechangeTask = (task : any) => {
      setTask(
       tasks.map((t) => {
